bbb9339b894cbf0a550f72661a7ceaa00aca9f74
RinBui <hai.thanhbui@2clicksolutions.com>
2019-11-05 11:04:42 +0700

    Updates

---
+++ precache-manifest.61b1e2e004716d8b868f4341d0f0208a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5135428018b782f19cefb63d55ac7d",
+    "url": "/react-immovables/index.html"
+  },
+  {
+    "revision": "eeed4cf0f2877b776c64",
+    "url": "/react-immovables/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "25e45ad69598eb97d72f",
+    "url": "/react-immovables/static/css/main.b9040a5a.chunk.css"
+  },
+  {
+    "revision": "eeed4cf0f2877b776c64",
+    "url": "/react-immovables/static/js/2.961a5808.chunk.js"
+  },
+  {
+    "revision": "9e02e7baffea103982f0",
+    "url": "/react-immovables/static/js/3.6def02df.chunk.js"
+  },
+  {
+    "revision": "25e45ad69598eb97d72f",
+    "url": "/react-immovables/static/js/main.96e9ca8a.chunk.js"
+  },
+  {
+    "revision": "fbadae3e811ff2d4cf41",
+    "url": "/react-immovables/static/js/runtime-main.ff557c74.js"
+  },
+  {
+    "revision": "60eac4c646b372f4594b482a3883523b",
+    "url": "/react-immovables/static/media/fondo1.60eac4c6.jpg"
+  },
+  {
+    "revision": "1ba7debd890be720ee12210e4252d8f8",
+    "url": "/react-immovables/static/media/fondo2.1ba7debd.jpg"
+  },
+  {
+    "revision": "caa660b752e538e26168bd12fad8fccd",
+    "url": "/react-immovables/static/media/fondo3.caa660b7.jpg"
+  },
+  {
+    "revision": "67abde11e7ec478207bb9412180488be",
+    "url": "/react-immovables/static/media/fondo4.67abde11.jpg"
+  },
+  {
+    "revision": "47978c5138ce0cc2c6734b3411beef39",
+    "url": "/react-immovables/static/media/fondo5.47978c51.jpg"
+  },
+  {
+    "revision": "18a41dd09d8555dd48d5828ebbe165f0",
+    "url": "/react-immovables/static/media/icomoon.18a41dd0.ttf"
+  },
+  {
+    "revision": "76cc813985423a87c451b993824f7bd8",
+    "url": "/react-immovables/static/media/icomoon.76cc8139.eot"
+  },
+  {
+    "revision": "87778cc453c39266098770025336ce22",
+    "url": "/react-immovables/static/media/icomoon.87778cc4.svg"
+  },
+  {
+    "revision": "b2fec262a91fea4d29550cbf1772f130",
+    "url": "/react-immovables/static/media/icomoon.b2fec262.woff"
+  },
+  {
+    "revision": "1e80540b18557641663e09dd5db19c40",
+    "url": "/react-immovables/static/media/login-bg.1e80540b.jpg"
+  },
+  {
+    "revision": "6589d122a2321ac8172b4136b1e0335e",
+    "url": "/react-immovables/static/media/logo-with-text.6589d122.svg"
+  },
+  {
+    "revision": "41c740a572b05453e91726adbf5fdfa9",
+    "url": "/react-immovables/static/media/logo.41c740a5.svg"
+  },
+  {
+    "revision": "07e1eba93150045ed13a1cc4994f6cc5",
+    "url": "/react-immovables/static/media/video-bg.07e1eba9.mp4"
+  },
+  {
+    "revision": "be11212b378960567b0519a635203076",
+    "url": "/react-immovables/static/media/video-poster.be11212b.jpg"
+  }
+]);
